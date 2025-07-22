ca6a2b9f6442887b5c112288a34caf0e62372004
Rukshan123 <rukzmadu@gmail.com>
2025-07-22 17:19:41 +0530

    bug fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
-import connectCloudinary from "./config/cloudinary.js";
+import connectCloudinary from "./config/cloundnary.js";
 
 // app configuration
 const app = express();
